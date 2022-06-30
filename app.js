293584f4b0c8763c1ba562482f7c10d24c26d01c
taewookim02 <taewookim02@gmail.com>
2022-07-01 00:37:20 +0800

    feat: add insert adjacent html

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+const ulList = document.querySelector('.ul-list');
+
+btn.addEventListener('click', function (e) {
+  const inputVal = input.value;
+  console.log(inputVal);
+  ulList.insertAdjacentHTML('beforeend', `<li>${inputVal}</li>`);
+});
+
+/*
+    <ul class="ul-list">
+      <li>groceries</li>
+      <li>KM assignment</li>
+      <li>SoC assignment</li>
+      <li>IBA assignment</li>
+      <li class="checked">move stuff</li>
+    </ul>
+*/
