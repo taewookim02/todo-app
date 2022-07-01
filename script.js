647e5e6d515f0db77a1a1ed9dccda800bd5c45c8
taewookim02 <taewookim02@gmail.com>
2022-07-01 23:45:40 +0800

    feat: class toggle when list item clicked

--- script.js
+++ script.js
@@ -3,13 +3,22 @@
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const ulList = document.querySelector('.ul-list');
+// const closeBtnUnicode = '/u00D7';
 
 btn.addEventListener('click', function (e) {
   const inputVal = input.value;
+  if (!inputVal) return;
+  input.value = '';
+  input.focus();
   console.log(inputVal);
   ulList.insertAdjacentHTML('beforeend', `<li>${inputVal}</li>`);
 });
 
+ulList.addEventListener('click', function (e) {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+  }
+});
 /*
     <ul class="ul-list">
       <li>groceries</li>
